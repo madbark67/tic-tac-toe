a178bf0ee2ac37d86578ac30b113c65e95c78288
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-02-18 22:26:24 -0500

    Complete tic tac toe game logic and error handling

--- board.rb
+++ board.rb
@@ -1,14 +1,27 @@
 class Board
+  WIN_COMBINATIONS = [
+    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+  ]
   def initialize
     @cells = Array.new(9,"-")
   end
 
   def win?
-    # map out all winnings combinations and check if they hit
+    WIN_COMBINATIONS.any? do |combo|
+      values = combo.map { |i| @cells[i]}
+      if !values.include?("-") && values.uniq.length == 1
+        return true
+      end
+    end
+    false
   end
 
   def draw?
     # only when board is full
+    if @cells.include?("-")
+      return false
+    end
+    true
   end
 
   def process_move(position, player_choice)
@@ -17,5 +30,15 @@ class Board
     end
     @cells[position] = player_choice
     true
+  end
+
+  def show_board 
+    puts 
+    puts "--+---+--"
+    @cells.each_slice(3) do |row|
+      puts row.join(" | ")
+      puts "--+---+--"
+    end
+    puts
   end
 end
--- game.rb
+++ game.rb
@@ -13,21 +13,28 @@ class Game
     @players << Player.new("Player 2", 'O')
 
     while !@board.win? && !@board.draw?
+
+      @board.show_board
+
       current_player = @players[@current_index]
       puts "#{current_player.name} select a spot you want to fill (1-9)"
       position = gets.chomp.to_i - 1
-      valid = board.process_move(position, current_player.choice) 
+      valid = @board.process_move(position, current_player.choice) 
       if !valid 
         puts "chosen spot is not valid"
         next
       end
+
+
+      if @board.win?
+        puts "The winner is #{current_player.name}"
+      elsif @board.draw?
+        puts "This game ends in a draw"
+      end
+
       @current_index = 1 - @current_index
     end
+    @board.show_board
 
-    if @board.win?
-      
-    elsif @board.draw?
-    
-    end
   end
 end
